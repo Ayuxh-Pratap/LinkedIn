af204f79a4621a856ed1d1a9989fd5b8625991e8
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-11-01 10:54:35 +0530

    login + logout

--- backend/lib/mailtrap.js
+++ backend/lib/mailtrap.js
@@ -1,11 +1,11 @@
-import { MailtrapClient } from "mailtrap";
+import { MailtrapClient } from "mailtrap"
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const TOKEN = process.env.MAILTRAP_TOKEN
 
-export const MailtrapClient = new MailtrapClient({
+export const mailtrapClient = new MailtrapClient({
     toke: TOKEN
 })
 
