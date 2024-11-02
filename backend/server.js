6cc386628cb8ec24be94c97bd329c642acf2d72e
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-11-02 08:59:27 +0530

    cloudinary config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/users", userRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
